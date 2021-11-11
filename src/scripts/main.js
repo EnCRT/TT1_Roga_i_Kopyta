d50f8c5d30066f4fc6c791e738bf34f3dfc11694
Dima Anosov <anosov.work@gmail.com>
2021-11-11 16:47:31 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,13 @@
 'use strict';
 
 const popupWindow = document.querySelector('.popup');
-const mainContent = document.querySelector('.main');
-const mainFooter = document.querySelector('.footer');
 const popupOpener = document.querySelector('.section-top__button');
 const popupCloser = document.querySelector('.popup__close-btn');
 
 popupOpener.onclick = function() {
   popupWindow.style.transform = 'translateX(0)';
-  popupWindow.style.position = 'relative';
-  mainContent.style.display = 'none';
-  mainFooter.style.display = 'none';
 };
 
 popupCloser.onclick = function() {
   popupWindow.style.transform = 'translateX(100%)';
-  popupWindow.style.position = 'fixed';
-  mainContent.style.display = 'unset';
-  mainFooter.style.display = 'unset';
 };
